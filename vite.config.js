5142a4c71c44ceeeea8240667558180b39ed47e6
Mihai <mihai.chirculescu@yahoo.com>
2024-03-21 10:05:04 +0200

    Run npm create vite@latest semantic-autocomplete -- --template react

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    lib: {
-      entry: 'src/SemanticAutocomplete.jsx',
-      name: 'SemanticAutocomplete',
-      // Output format options: "umd", "iife", "es"
-      formats: ['es', 'umd'],
-      fileName: (format) => `semantic-autocomplete.${format}.js`
-    },
-    rollupOptions: {
-      // Explicitly specify external dependencies (if any)
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
-      }
-    },
-  }
-});
+})
